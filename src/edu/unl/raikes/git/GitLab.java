f442e1af76966810809ba74d1c933d94b1581a82
10625639 <cmoloney2@huskers.unl.edu>
2024-02-07 11:14:35 -0600

    modified main and added GitLabHelper

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -3,7 +3,8 @@ package edu.unl.raikes.git;
 public class GitLab {
 
     public static void main(String[] args) {
-        System.out.println("This is a lesson on Git");
+        GitLabHelper helper = new GitLabHelper("Ryan", "Charles");
+        helper.PrintWelcomeMessage();
         
     }
 }
---
+++ src/edu/unl/raikes/git/GitLabHelper.java
@@ -0,0 +1,16 @@
+package edu.unl.raikes.git;
+
+public class GitLabHelper {
+
+    private String person1;
+    private String person2;
+    
+    GitLabHelper(String person1, String person2) {
+        this.person1 = person1;
+        this.person2 = person2;
+    }
+    
+    public void PrintWelcomeMessage() {
+        System.out.print("Git lab by: " + person1 + " and " + person2);
+}
+}

b0344abf0b63329c46ed2d14592c7b61558ce1ed
Ryan Flatley <rflatley2@huskers.unl.edu>
2024-02-07 11:19:26 -0600

    New push

--- src/edu/unl/raikes/git/GitLabHelper.java
+++ src/edu/unl/raikes/git/GitLabHelper.java
@@ -1,16 +1,18 @@
 package edu.unl.raikes.git;
 
+import java.util.Arrays;
+
 public class GitLabHelper {
 
-    private String person1;
-    private String person2;
-    
-    GitLabHelper(String person1, String person2) {
-        this.person1 = person1;
-        this.person2 = person2;
+    private String[] persons;
+        GitLabHelper(String[] persons) {
+    this.persons = persons;
     }
     
     public void PrintWelcomeMessage() {
-        System.out.print("Git lab by: " + person1 + " and " + person2);
+        System.out.print("Git lab by: " + Arrays.toString(persons));
+    }
+    
+
 }
-}
+
